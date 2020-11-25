3d0d8381c05aef2ac3fb6991dbdad31dc3a5cdfa
Alon (PC) <downtown2u@gmail.com>
2020-11-25 22:53:59 +0200

    Added the compare method to the api

--- api.py
+++ api.py
@@ -4,7 +4,104 @@ import requests
 from exceptions import *
 
 
-class API:
+class SingleDayData:
+    """ Represents Covid data about a single day, from a single country. """
+
+    __raw_data_dict = None
+    __VALID_DICT_KEYS = {"Country", "CountryCode", "Province", "City", "CityCode",
+                         "Lat", "Lon", "Confirmed", "Deaths", "Recovered", "Active", "Date"}
+
+    def __init__(self, data_dict: dict):
+
+        self.__check_valid_data(data_dict)
+        self.__raw_data_dict = data_dict
+
+    @classmethod
+    def __check_valid_data(cls, data_dict: dict):
+        """ Checks if the given dict is a valid data dict.
+        If not, raises an error. """
+
+        if len(data_dict) != len(cls.__VALID_DICT_KEYS):
+            cls.__raise_data_error()
+
+        if set(data_dict.keys()) != cls.__VALID_DICT_KEYS:
+            cls.__raise_data_error()
+
+    @staticmethod
+    def __raise_data_error():
+        raise ValueError("The given data dict is not a valid one.")
+
+    @property
+    def confirmed_cases(self):
+        return self.__raw_data_dict["Confirmed"]
+
+    @property
+    def deaths(self):
+        return self.__raw_data_dict["Deaths"]
+
+    @property
+    def recovered_cases(self):
+        return self.__raw_data_dict["Recovered"]
+
+    @property
+    def active_cases(self):
+        return self.__raw_data_dict["Active"]
+
+    @property
+    def date(self):
+        return self.__raw_data_dict["Date"]
+
+
+class DayDataDiff:
+
+    """ Compare two `SingleDayData` objects. """
+
+    def __init__(self, newest: SingleDayData, older: SingleDayData):
+        self.__newest = newest
+        self.__older = older
+
+    @staticmethod
+    def __genrate_diff(newest_data, older_data):
+        return newest_data / older_data
+
+    @staticmethod
+    def __generate_percentage_diff(newest_data, older_data):
+        return (newest_data / older_data - 1) * 100
+
+    @property
+    def confirmed_diff(self):
+        return self.__genrate_diff(self.__newest.confirmed_cases, self.__older.confirmed_cases)
+
+    @property
+    def confirmed_percentage_diff(self):
+        return self.__generate_percentage_diff(self.__newest.confirmed_cases, self.__older.confirmed_cases)
+
+    @property
+    def deaths_diff(self):
+        return self.__genrate_diff(self.__newest.deaths, self.__older.deaths)
+
+    @property
+    def deaths_percentage_diff(self):
+        return self.__generate_percentage_diff(self.__newest.deaths, self.__older.deaths)
+
+    @property
+    def recovered_diff(self):
+        return self.__genrate_diff(self.__newest.recovered_cases, self.__older.recovered_cases)
+
+    @property
+    def recovered_percentage_diff(self):
+        return self.__generate_percentage_diff(self.__newest.recovered_cases, self.__older.recovered_cases)
+
+    @property
+    def active_diff(self):
+        return self.__genrate_diff(self.__newest.active_cases, self.__older.active_cases)
+
+    @property
+    def active_percentage_diff(self):
+        return self.__generate_percentage_diff(self.__newest.active_cases, self.__older.active_cases)
+
+
+class Covid19API:
 
     MAIN_URL = URL("https://api.covid19api.com")
 
@@ -60,50 +157,8 @@ class API:
         """ Returns the data Covid data TODAY from the given country. """
         return cls.get_stats(country, last_x_days=1)[0]
 
-
-class SingleDayData:
-    """ Represents Covid data about a single day, from a single country. """
-
-    __raw_data_dict = None
-    __VALID_DICT_KEYS = {"Country", "CountryCode", "Province", "City", "CityCode",
-                         "Lat", "Lon", "Confirmed", "Deaths", "Recovered", "Active", "Date"}
-
-    def __init__(self, data_dict: dict):
-
-        self.__check_valid_data(data_dict)
-        self.__raw_data_dict = data_dict
-
-    @classmethod
-    def __check_valid_data(cls, data_dict: dict):
-        """ Checks if the given dict is a valid data dict.
-        If not, raises an error. """
-
-        if len(data_dict) != len(cls.__VALID_DICT_KEYS):
-            cls.__raise_data_error()
-
-        if set(data_dict.keys()) != cls.__VALID_DICT_KEYS:
-            cls.__raise_data_error()
-
     @staticmethod
-    def __raise_data_error():
-        raise ValueError("The given data dict is not a valid one.")
-
-    @property
-    def confirmed_cases(self):
-        return self.__raw_data_dict["Confirmed"]
-
-    @property
-    def deaths(self):
-        return self.__raw_data_dict["Deaths"]
-
-    @property
-    def recovered_cases(self):
-        return self.__raw_data_dict["Recovered"]
-
-    @property
-    def active_cases(self):
-        return self.__raw_data_dict["Active"]
-
-    @property
-    def date(self):
-        return self.__raw_data_dict["Date"]
+    def compare_day_data(newest: SingleDayData, older: SingleDayData) -> DayDataDiff:
+        """ Compares the two day data objects, and returns an `DayDataDiff`
+        instance. """
+        return DayDataDiff(newest=newest, older=older)
