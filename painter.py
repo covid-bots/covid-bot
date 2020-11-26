eb16e2c8b97dbd89492691e30c8f546bd926d528
Alon (PC) <downtown2u@gmail.com>
2020-11-26 09:56:27 +0200

    Created method generate_base_img to painter

--- painter.py
+++ painter.py
@@ -1,52 +1,43 @@
 from typing import Tuple
 from PIL import Image, ImageDraw
 import math
+from os import path, listdir
+import random
 
 
 class ImageGenerator:
 
     BACKGROUND_COLORS = {
-        0: {
-            "r": 105,
-            "g": 191,
-            "b": 226,
-        },
-        100: {
-            "r": 105,
-            "g": 226,
-            "b": 185,
-        },
-        250: {
-            "r": 111,
-            "g": 226,
-            "b": 105,
-        },
-        500: {
-            "r": 168,
-            "g": 226,
-            "b": 105,
-        },
-        1000: {
-            "r": 246,
-            "g": 171,
-            "b": 65,
-        },
-        2500: {
-            "r": 246,
-            "g": 120,
-            "b": 65,
-        },
-        5000: {
-            "r": 246,
-            "g": 65,
-            "b": 65,
-        },
-        10000: {
-            "r": 140,
-            "g": 29,
-            "b": 20,
-        }
+        0:     (105, 191, 226),
+        100:   (105, 226, 185),
+        250:   (111, 226, 105),
+        500:   (168, 226, 105),
+        1000:  (246, 171, 65),
+        2500:  (246, 120, 65),
+        5000:  (246, 65,  65),
+        10000: (140, 29,  20),
     }
+
+    RANDOM_BACKGROUND_IMAGES_FOLDER = path.join("assets", "backgrounds")
+
+    @classmethod
+    def generate_base_img(cls, new_cases: int):
+        """ Returns an `Image` object that represents the background of the image. """
+
+        # Choose random background
+        files = listdir(cls.RANDOM_BACKGROUND_IMAGES_FOLDER)
+        random_file = random.choice(files)
+
+        # Load random background
+        random_img = Image.open(
+            path.join(cls.RANDOM_BACKGROUND_IMAGES_FOLDER, random_file))
+
+        # Load background color image
+        bg_color = cls.get_background_color(new_cases)
+        background_img = Image.new("RGBA", random_img.size, color=bg_color)
+
+        # Join two images
+        return Image.alpha_composite(background_img, random_img)
 
     @classmethod
     def get_background_color(cls, new_cases: int):
@@ -90,13 +81,15 @@ class ImageGenerator:
         high_neighbor_force = new_cases_fixed / neighbors_delta
         low_neighbor_force = 1 - high_neighbor_force
 
-        return {color_key:
-                int(
-                    (low_neighbor_color[color_key] * low_neighbor_force) +
-                    (high_neighbor_color[color_key] * high_neighbor_force)
-                )
-                for color_key in low_neighbor_color.keys()
-                }
+        new_color = [
+            int(
+                (low_color_elem * low_neighbor_force) +
+                (high_color_elem * high_neighbor_force)
+            )
+            for low_color_elem, high_color_elem in zip(low_neighbor_color, high_neighbor_color)
+        ]
+
+        return tuple(new_color)
 
     @classmethod
     def test_background_gradint(cls, from_: int, to: int, jumps: int = 1):
@@ -110,14 +103,10 @@ class ImageGenerator:
         for cur_index, cur_color_num in enumerate(range(from_, to, jumps)):
 
             color = cls.get_background_color(cur_color_num)
-            color_tuple = (color["r"], color["g"], color["b"])
 
             p1 = (cur_index, 0)
             p2 = (cur_index, height)
 
-            draw.line([p1, p2], fill=color_tuple, width=1)
+            draw.line([p1, p2], fill=color, width=1)
 
         return img
-
-
-ImageGenerator.test_background_gradint(0, 5000).show()
