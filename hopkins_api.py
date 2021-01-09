3ea98d9911d6f67deeeaa586d47f84ab0f53e3ec
Alon (PC) <downtown2u@gmail.com>
2021-01-09 22:39:03 +0200

    📝 Added comments and documentation to Hopkins API methods

--- hopkins_api.py
+++ hopkins_api.py
@@ -5,30 +5,27 @@ You can find more information about the API at https://github.com/CSSEGISandData
 """
 
 import typing
-import csv
-import requests
 import datetime
 import logging
+import csv
+import requests
 
 
 class CountryData:
+    """ An object that stores a collection of Covid19 data of
+    a specific country. """
 
     def __init__(self,
-                 name: str,
                  data: typing.List[dict],
                  ):
-        self.__name = name
+        """ Constructed by the `CovidHistoryDatabase` object, and should not be
+        initialized by the user. Recives a data dict, and stores it. """
         self.__data = data
 
     @property
     def data(self,) -> typing.List[dict]:
         """ Returns the raw data that is saved in the instance. """
         return self.__data
-
-    @property
-    def name(self,) -> str:
-        """ The country name, as a string. """
-        return self.__name
 
     @property
     def confirmed_each_day(self,) -> typing.List[int]:
@@ -41,10 +38,12 @@ class CountryData:
 
     @property
     def confirmed(self,) -> int:
+        """ The total confirmed Covid19 cases up to date. """
         return self.confirmed_each_day[-1]
 
     @property
     def confirmed_yesterday(self,) -> int:
+        """ The total confirmed Covid19 cases, up until yesterday. """
         return self.confirmed_each_day[-2]
 
     @property
@@ -58,14 +57,17 @@ class CountryData:
 
     @property
     def deaths(self, ) -> int:
+        """ The total death cases due to Covid19 up to date. """
         return self.deaths_each_day[-1]
 
     @property
     def deaths_yesterday(self,) -> int:
+        """ The total death cases due to Covid19 up until yesterday. """
         return self.deaths_each_day[-2]
 
     @property
     def deaths_diff_today(self,) -> int:
+        """ The amount of new death cases recorded between today and yesterday. """
         return self.deaths_each_day[-1] - self.deaths_each_day[-2]
 
     @property
@@ -79,18 +81,25 @@ class CountryData:
 
     @property
     def recovered(self,) -> int:
+        """ The total recorded recovered Covid19 cases up to date. """
         return self.recovered_each_day[-1]
 
     @property
     def recovered_yesterday(self,) -> int:
+        """ The total recorded recovered Covid19 cases up until to yesterday. """
         return self.recovered_each_day[-2]
 
     @property
     def recovered_diff_today(self,) -> int:
+        """ The delta (difference) in recovered cases between today and yesterday. """
         return self.recovered_each_day[-1] - self.recovered_each_day[-2]
 
     @property
     def active_each_day(self,) -> typing.List[int]:
+        """ A list of integers, where each cell represents the amount of
+        active cases in the date that the cell represents. The first
+        cell represents the 22nd of January 2020, and the last cell
+        represent the newest data (today / yesterday). """
         return [
             confirmed - recovered - deaths
             for confirmed, recovered, deaths
@@ -99,14 +108,17 @@ class CountryData:
 
     @property
     def active(self,) -> int:
+        """ The amount of active cases in the newest data (today / yesterday)."""
         return self.active_each_day[-1]
 
     @property
     def active_yesterday(self,) -> int:
+        """ The amount of active cases in the represented country, yesterday. """
         return self.active_each_day[-2]
 
     @property
     def active_diff_today(self,) -> int:
+        """ The delta (difference) in active cases between today and yesterday. """
         return self.active_each_day[-1] - self.active_each_day[-2]
 
     @property
@@ -131,6 +143,7 @@ class CountryData:
 
     @property
     def new_cases_yesterday(self,) -> int:
+        """ The amount of new cases recorded yesterday. """
         return self.new_cases_each_day[-2]
 
     def __new_cases_x_days_averages(self, days: int) -> typing.List[float]:
@@ -202,10 +215,14 @@ class CountryData:
 
     @property
     def r_value_yesterday(self,) -> float:
+        """ The R value that represents the data from yesterday. """
         return self.r_values_each_day[-2]
 
 
 class ApiFromCsv:
+    """ Represents an API endpoint that contains data represented by a 
+    csv table. Has a couple of methods that downloads and manipulates the
+    data. """
 
     def __init__(self, url: str):
         """ When initialized, requests data from the API, downloads the csv sheet
@@ -239,7 +256,7 @@ class ApiFromCsv:
         # Check if data loaded correctly
         if response.status_code != 200:
             raise RequestAPIError(
-                f"{self.API_UR}:\nResponse status {response.status_code}.")
+                f"{self.API_URL}:\nResponse status {response.status_code}.")
 
         # convert downloaded content (in binary) to a 2d list with rows and
         # columns not very efficient, saves the whole downloaded spreadsheet
@@ -265,14 +282,15 @@ class ApiFromCsv:
             for row in content
         ]
 
-    def __change_item_type(self, item: str) -> typing.Any:
+    @staticmethod
+    def __change_item_type(item: str) -> typing.Any:
         """ Convert the given string into an integer, float, leaves it a string,
         or even to `None` if the string is empty. """
 
         # try converting to integer.
         # if not possible, try converting to float
         # if not possible, try converting to string
-        # if not possible (or empty string), set to `None`
+        # if not possible (or empty string), returns `None`
 
         try:
             return int(item)
@@ -287,11 +305,8 @@ class ApiFromCsv:
                 if str(item):
                     return str(item)
 
-                else:
-                    return None
-
-    def _content_to_list_of_dicts(self,
-                                  content: typing.List[typing.List[typing.Any]],
+    @staticmethod
+    def _content_to_list_of_dicts(content: typing.List[typing.List[typing.Any]],
                                   headers: typing.List[str],
                                   ) -> typing.List[typing.Dict[str, typing.Any]]:
         """ Converts the 2d list database content into a list of dictionaries.
@@ -309,33 +324,47 @@ class ApiFromCsv:
         """ Saves the data into a spreadsheet in the given file path, as downloaded
         from the API (not including the modifications of the script). """
 
-        with open(path, 'wb') as f:
-            f.write(self.__raw_response_content)
+        with open(path, 'wb') as file:
+            file.write(self.__raw_response_content)
 
 
 class DateHistoryCvsApi(ApiFromCsv):
+    """ Represents one Covid19 history endpoint. It can load and
+    download the information from the endpoint url, manipulate
+    and even save it. """
 
     def __init__(self,
                  url: str,
                  id_index: int = 0,
                  ):
+        # Calling the super constructor will download the data
+        # and save it in the `_content` and `_header` properties.
+        # Usually the data is large (a couple of MB), and will
+        # take a couple of seconds to download.
         super().__init__(url)
 
         logging.info("Prossecing the downloaded information...")
 
+        # Converting the raw downloaded data and storing it
+        # inside of a list of dictionaries
         date_data = self.__generate_date_data(id_index)
         self.__date_data = self.__squash_data_by_id(date_data)
 
     def __generate_date_data(self, id_index: int,):
+        """ Converts the 'raw' downloaded data into a list of dictionaries.
+        `id_index` is the index of the column that will be used to generate
+        the `id` value for each dictionary. """
 
         dates, not_date_indexes = self.__generate_dates()
 
         return [
             {
                 'id': row_data[id_index],
-                'data': self.__generate_row_data(row_data,
-                                                 dates,
-                                                 not_date_indexes),
+                'data': self.__generate_row_data(
+                    row_data,
+                    dates,
+                    not_date_indexes,
+                ),
             }
 
             for row_data in self._content
@@ -346,6 +375,17 @@ class DateHistoryCvsApi(ApiFromCsv):
                             dates: list,
                             not_date_indexes: list,
                             ):
+        """ Recives three lists:
+        1. `row_data`: A list of the actual data from the current row.
+        2. `dates`: A list of all of the valid dates from the downloaded data.
+        3. `not_date_indexes`: A list of indexes in the `row_data` list, in which
+            the value is not represented by a specific date from the `dates` list.
+
+        This method will iterate over the list of dates and the row data, and will
+        match each data item to a specific date, and will return a list of
+        dictionaries, where in each dictionary the 'date' will contain the date,
+        and the 'value' will contain the value from the `row_data` that matches
+        the date. """
 
         dates = iter(dates)
         data = list()
@@ -359,28 +399,37 @@ class DateHistoryCvsApi(ApiFromCsv):
 
         return data
 
-    def __squash_data_by_id(self, data):
+    def __squash_data_by_id(self, data: typing.List[dict]):
+        """ Recives a list of dictionaries that contain two keys - `id` and
+        `data`. Iterates over teh list, and and merges the data of dictionaries
+        that share the same `id`. """
 
         visited_ids = set()
         new_data = list()
 
-        for cur_index, cur_data in enumerate(data):
+        for cur_data in data:
             if cur_data['id'] in visited_ids:
+                # if current id already visited -> needs to be merged!
 
+                # finds the already visited object
                 already_visited_index = next(
                     new_data.index(cur_visited)
                     for cur_visited in new_data
                     if cur_visited['id'] == cur_data['id']
                 )
 
+                # updates the current data with the merged data
                 cur_data['data'] = self.__merge_same_ids(
                     cur_data,
                     data[already_visited_index],
                 )
 
+                # removes the already saves data from the visited list
                 new_data.pop(already_visited_index)
 
             else:
+                # if id not visited yet
+                # gets added to the visited list!
                 visited_ids.add(cur_data['id'])
 
             new_data.append(cur_data)
@@ -388,7 +437,12 @@ class DateHistoryCvsApi(ApiFromCsv):
         return new_data
 
     def __merge_same_ids(self, data_one, data_two,):
+        """ Recives two data objects (a list of dictionaties). Each dictionary
+        has a `date` field that contains a `datetime.date` instance. This
+        methods iterates over both lists, and creates a new list in which the
+        data from both lists are merged into one list. """
 
+        # generates the list of dates for each dataset
         one_dates = [cur['date'] for cur in data_one['data']]
         two_dates = [cur['date'] for cur in data_two['data']]
 
@@ -397,10 +451,13 @@ class DateHistoryCvsApi(ApiFromCsv):
 
         new_data = list()
 
+        # loop over the list of combined dates
         for date in dates:
 
             value = 0
 
+            # tries to find data from first list that matches the
+            # current date.
             try:
                 value += next(
                     cur_data['value']
@@ -409,8 +466,12 @@ class DateHistoryCvsApi(ApiFromCsv):
                 )
 
             except StopIteration:
+                # if current date doesn't appear in any of the values in
+                # the first list
                 pass
 
+            # tries to find data from the second that matches the
+            # current date.
             try:
                 value += next(
                     cur_data['value']
@@ -419,8 +480,11 @@ class DateHistoryCvsApi(ApiFromCsv):
                 )
 
             except StopIteration:
+                # if current date doesn't appear in any of the values in
+                # the second list
                 pass
 
+            # adds the new combined data the the `new_data` list
             new_data.append({
                 'date': date,
                 'value': value,
@@ -428,48 +492,69 @@ class DateHistoryCvsApi(ApiFromCsv):
 
         return new_data
 
-    def __generate_dates(self,):
+    def __generate_dates(self,) -> typing.Tuple[typing.List[datetime.date], typing.List[int]]:
+        """ Generates a list of `datetime.date` instances from the list
+        of data headers (the `self._header` property). In addition, returns
+        a list of indexes that represents the headers that are NOT dates. """
 
         not_date_indexes = set()
         dates = list()
 
-        for index, header in enumerate(self._headers):
-            try:
-                date = self.__string_to_date(header)
+        for index, header in self._headers:
+            date = self.__string_to_date(header)
+
+            if date is not None:
                 dates.append(date)
 
-            except Exception:
+            else:
                 not_date_indexes.add(index)
 
         return dates, not_date_indexes
 
-    def __string_to_date(self, date_string: str) -> datetime.date:
+    @staticmethod
+    def __string_to_date(date_string: str) -> datetime.date:
         """ Converts the date string recvied by the API into a `datetime.date`
-        instance, and returns it. """
+        instance, and returns it. If can't convert the given string into
+        a `date` instance, returns `None`. """
 
-        month, day, year = date_string.split('/')
-        month, day, year = (int(value) for value in (month, day, year))
+        try:
+            month, day, year = date_string.split('/')
+            month, day, year = (int(value) for value in (month, day, year))
 
-        # API provides years with only 2 less significant digits.
-        # For example, if the year is `2021`, the api will return only `21`.
-        # This adds the `2000` part (:
-        today = datetime.date.today()
-        century = int(today.year / 100)
-        year += century * 100
+            # API provides years with only 2 less significant digits.
+            # For example, if the year is `2021`, the api will return only `21`.
+            # This adds the `2000` part (:
+            today = datetime.date.today()
+            century = int(today.year / 100)
+            year += century * 100
 
-        return datetime.date(day=day, month=month, year=year,)
+            return datetime.date(day=day, month=month, year=year,)
 
-    def all_data(self,):
+        except Exception:  # pylint: disable=broad-except
+            # If can't convert given string to date, returns `None`
+            return None
+
+    def all_data(self,) -> typing.List[dict]:
+        """ The raw total data. """
         return self.__date_data
 
-    def data_by_id(self, data_id: str):
-        return next(
-            data['data']
-            for data in self.all_data()
-            if data['id'] == data_id
-        )
+    def data_by_id(self, data_id: str) -> list:
+        """ Returns only the data by the given id. Use the `all_data` method
+        to get a list of all id's. """
 
-    def data_by_date(self, date: datetime.date):
+        try:
+            return next(
+                data['data']
+                for data in self.all_data()
+                if data['id'] == data_id
+            )
+
+        except StopIteration:
+            raise TypeError(f"Invalid id {data_id}")
+
+    def data_by_date(self, date: datetime.date) -> typing.List[dict]:
+        """ Returns a list of dictionaries that only contain data from
+        the given date. """
 
         data_list = list()
 
@@ -484,6 +569,7 @@ class DateHistoryCvsApi(ApiFromCsv):
         return data_list
 
 
+# pylint: disable=line-too-long
 # Source: JHU CSSE COVID-19 Data
 # https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series
 COVID_DEATHS_GLOBAL_HISTORY_ENDPOINT = r'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'
@@ -574,10 +660,7 @@ class CovidHistoryDatabase:
         )
 
     def country(self, country: str):
-        return CountryData(
-            name=country,
-            data=self.country_data(country),
-        )
+        return CountryData(data=self.country_data(country))
 
     def date_data(self, date: datetime.date):
 
